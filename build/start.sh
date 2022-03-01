f3fe50e98da20213beca9412337c6c4985464356
root <root@localhost.localdomain>
2022-03-01 16:43:45 +0800

    容器启动脚本

---
+++ build/start.sh
@@ -0,0 +1,21 @@
+#进入工作目录
+cd /OLAINDEX
+
+#判断是否安装
+if [ -d "vendor" ];
+then
+     echo "OLAINDEX is already installed"
+else
+git clone --depth 1 https://github.com/WangNingkai/OLAINDEX.git . && \
+composer install -vvv && \
+composer run install-app 
+fi
+
+#配置目录权限
+addgroup -g 900 -S olaindex && \
+adduser -h /OLAINDEX -s /bin/sh -G olaindex -u 900 -S olaindex && \
+chown -R olaindex:olaindex /OLAINDEX && \
+chmod 755 /OLAINDEX/storage
+
+#开启PHP服务
+php artisan serve --host=0.0.0.0 --port=8000 --tries=0 --no-interaction
